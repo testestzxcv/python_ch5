b9bb20628efe922e673e4e858e5edb800ece3d42
james <illuillu55@hotmail.com>
2018-06-28 01:04:55 +0900

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\core\Documents\PycharmProjects\python_ch5\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
